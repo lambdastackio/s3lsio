d5c468652abf19fc415fd09f3866091dc26e6ee1
Chris Jones <cj@hanscj.com>
2016-10-13 18:02:55 -0400

    Added time duration and started benchmarking

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,7 @@ extern crate pbr;
 extern crate toml;
 extern crate md5;
 
+use std::time::{Duration, SystemTime};
 use std::io::{self, Write};
 use std::env;
 use std::path::{Path, PathBuf};
@@ -60,6 +61,7 @@ use url::Url;
 use aws_sdk_rust::aws::errors::s3::S3Error;
 use aws_sdk_rust::aws::s3::endpoint::*;
 use aws_sdk_rust::aws::s3::s3client::S3Client;
+use aws_sdk_rust::aws::common::common::Operation;
 use aws_sdk_rust::aws::common::region::Region;
 use aws_sdk_rust::aws::common::credentials::{AwsCredentialsProvider, DefaultCredentialsProvider};
 use aws_sdk_rust::aws::common::request::DispatchSignedRequest;
@@ -132,6 +134,28 @@ pub struct Output {
     pub color: term::color::Color,
 }
 
+/// Allows for duration tracking of operations. You should not track time of this app running but
+/// of each operation and then the summation of the durations plus latency etc.
+///
+/*
+#[derive(Debug, Clone)]
+pub struct Operation {
+    /// request
+    pub request: String,
+    /// Endpoint URL
+    pub endpoint: String,
+    /// GET, PUT, DELETE...
+    pub verb: String,
+    /// If the operation succeeded or not
+    pub success: bool,
+    /// HTTP return code
+    pub code: u16,
+    /// System time of beginning of actual operation (not time spent on condition logic, etc)
+    pub start_time: SystemTime,
+    /// System time of end of actual operation (not time spent on condition logic, etc)
+    pub end_time: SystemTime,
+}
+*/
 /// Client structure holds a reference to the S3Client which also implements two traits:
 /// AwsCredentialsProvider and DispatchSignedRequest
 /// Since S3Client struct is takes those two traits as parameters then ALL functions called
@@ -144,15 +168,18 @@ pub struct Client<'a, P: 'a, D: 'a>
           D: DispatchSignedRequest, // T: Write,
 {
     pub s3client: &'a mut S3Client<P, D>,
+    pub operations: Option<&'a mut Vec<Operation>>,
+    pub config: &'a mut config::Config, // pub pbr: ProgressBar<T>,
     pub error: Error,
     pub output: Output,
     pub is_quiet: bool,
-    pub config: &'a mut config::Config, // pub pbr: ProgressBar<T>,
+    pub is_time: bool,
 }
 
 fn main() {
     // Gets overridden by cli option
     let mut is_quiet: bool = false;
+    let mut is_time: bool = false;
 
     env_logger::init().unwrap();
 
@@ -181,6 +208,11 @@ fn main() {
         is_quiet = true;
     }
 
+    // If the -t or --time flag was passed then track operation time
+    if matches.is_present("time") {
+        is_time = true;
+    }
+
     // NOTE: Get parameters or config for region, signature etc
     // Safe to unwrap since a default value is passed in. If a panic occurs then the environment
     // does not support a home directory.
@@ -205,6 +237,7 @@ fn main() {
     let ep_str = matches.value_of("endpoint");
     let proxy_str = matches.value_of("proxy");
     let signature_str = matches.value_of("signature");
+    let bench = matches.value_of("bench");
 
     let output = match matches.value_of("output").unwrap().to_string().to_lowercase().as_ref() {
         "json" => OutputFormat::JSON,
@@ -216,6 +249,7 @@ fn main() {
         "simple" => OutputFormat::Simple,
         _ => OutputFormat::PrettyJSON,
     };
+
     let output_color = match matches.value_of("output-color").unwrap().to_string().to_lowercase().as_ref() {
         "green" => term::color::GREEN,
         "red" => term::color::RED,
@@ -265,9 +299,19 @@ fn main() {
                                  Some(format!("s3lsio - {}", version)));
 
     let mut s3client = S3Client::new(provider, endpoint);
+    let mut operation: Vec<Operation>;
+    let operations: Option<&mut Vec<Operation>>;
+    if bench.is_some() {
+        operation = Vec::new();
+        operations = Some(&mut operation);
+    } else {
+        operations = None;
+    }
 
     let mut client = Client {
         s3client: &mut s3client,
+        config: &mut config,
+        operations: operations,
         error: Error {
             format: OutputFormat::Serialize,
             color: term::color::RED,
@@ -277,33 +321,51 @@ fn main() {
             color: output_color,
         },
         is_quiet: is_quiet,
-        config: &mut config,
+        is_time: is_time,
     };
 
-    // Check which subcomamnd the user ran...
-    let res = match matches.subcommand() {
-        ("abort", Some(sub_matches)) => commands::commands(sub_matches, Commands::abort, &mut client),
-        ("acl", Some(sub_matches)) => commands::commands(sub_matches, Commands::acl, &mut client),
-        ("get", Some(sub_matches)) => commands::commands(sub_matches, Commands::get, &mut client),
-        ("head", Some(sub_matches)) => commands::commands(sub_matches, Commands::head, &mut client),
-        ("ls", Some(sub_matches)) => commands::commands(sub_matches, Commands::ls, &mut client),
-        ("mb", Some(sub_matches)) => commands::commands(sub_matches, Commands::mb, &mut client),
-        ("put", Some(sub_matches)) => commands::commands(sub_matches, Commands::put, &mut client),
-        ("range", Some(sub_matches)) => commands::commands(sub_matches, Commands::range, &mut client),
-        ("rb", Some(sub_matches)) => commands::commands(sub_matches, Commands::rb, &mut client),
-        ("rm", Some(sub_matches)) => commands::commands(sub_matches, Commands::rm, &mut client),
-        ("setacl", Some(sub_matches)) => commands::commands(sub_matches, Commands::setacl, &mut client),
-        ("setver", Some(sub_matches)) => commands::commands(sub_matches, Commands::setver, &mut client),
-        ("ver", Some(sub_matches)) => commands::commands(sub_matches, Commands::ver, &mut client),
-        (e, _) => {
-            println_color_quiet!(client.is_quiet, term::color::RED, "{}", e);
-            Err(S3Error::new("A valid instruction is required"))
-        },
-    };
+    if bench.is_some() {
+        let res = match matches.subcommand() {
+            ("get", Some(sub_matches)) => commands::commands(sub_matches, Commands::get, &mut client),
+            ("put", Some(sub_matches)) => commands::commands(sub_matches, Commands::put, &mut client),
+            ("range", Some(sub_matches)) => commands::commands(sub_matches, Commands::range, &mut client),
+            ("rm", Some(sub_matches)) => commands::commands(sub_matches, Commands::rm, &mut client),
+            (e, _) => {
+                println_color_quiet!(client.is_quiet, term::color::RED, "{}", e);
+                Err(S3Error::new("A valid benchmarking instruction is required"))
+            },
+        };
+        if let Err(e) = res {
+            println_color_quiet!(client.is_quiet, term::color::RED, "An error occured: {}", e);
+            println_color_quiet!(client.is_quiet, term::color::RED, "{}", matches.usage());
+            ::std::process::exit(1);
+        }
+    } else {
+        // Check which subcomamnd the user wants to run...
+        let res = match matches.subcommand() {
+            ("abort", Some(sub_matches)) => commands::commands(sub_matches, Commands::abort, &mut client),
+            ("acl", Some(sub_matches)) => commands::commands(sub_matches, Commands::acl, &mut client),
+            ("get", Some(sub_matches)) => commands::commands(sub_matches, Commands::get, &mut client),
+            ("head", Some(sub_matches)) => commands::commands(sub_matches, Commands::head, &mut client),
+            ("ls", Some(sub_matches)) => commands::commands(sub_matches, Commands::ls, &mut client),
+            ("mb", Some(sub_matches)) => commands::commands(sub_matches, Commands::mb, &mut client),
+            ("put", Some(sub_matches)) => commands::commands(sub_matches, Commands::put, &mut client),
+            ("range", Some(sub_matches)) => commands::commands(sub_matches, Commands::range, &mut client),
+            ("rb", Some(sub_matches)) => commands::commands(sub_matches, Commands::rb, &mut client),
+            ("rm", Some(sub_matches)) => commands::commands(sub_matches, Commands::rm, &mut client),
+            ("setacl", Some(sub_matches)) => commands::commands(sub_matches, Commands::setacl, &mut client),
+            ("setver", Some(sub_matches)) => commands::commands(sub_matches, Commands::setver, &mut client),
+            ("ver", Some(sub_matches)) => commands::commands(sub_matches, Commands::ver, &mut client),
+            (e, _) => {
+                println_color_quiet!(client.is_quiet, term::color::RED, "{}", e);
+                Err(S3Error::new("A valid instruction is required"))
+            },
+        };
 
-    if let Err(e) = res {
-        println_color_quiet!(client.is_quiet, term::color::RED, "An error occured: {}", e);
-        println_color_quiet!(client.is_quiet, term::color::RED, "{}", matches.usage());
-        ::std::process::exit(1);
+        if let Err(e) = res {
+            println_color_quiet!(client.is_quiet, term::color::RED, "An error occured: {}", e);
+            println_color_quiet!(client.is_quiet, term::color::RED, "{}", matches.usage());
+            ::std::process::exit(1);
+        }
     }
 }
