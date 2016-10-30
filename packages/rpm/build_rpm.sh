b5098a390a853f139f1d00542691cc04f23d367e
Chris Jones <cj@hanscj.com>
2016-10-30 15:35:34 -0400

    Updated RPM SPEC

--- packages/rpm/build_rpm.sh
+++ packages/rpm/build_rpm.sh
@@ -16,17 +16,38 @@
 
 # NOTE: MUST be ran as root
 
+REPO_BASE=$(git rev-parse --show-toplevel)
+USER=$(whoami)
+
+cargo build --release
+
+# Has to be as root...
+APP=s3lsio
+ARCH=x86_64
 BASE=/root/rpmbuild
-SPEC=s3lsio.spec
+SPEC=$APP.spec
+RELEASE=target/release/$APP
 
-yum update -y
-yum install -y rpm-build
+sudo yum update -y
+sudo yum install -y rpm-build
 
-mkdir -p $BASE/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-cp specs/$SPEC $BASE/SPECS/$SPEC
+# Remove any older versions...
+sudo rm -rf $BASE/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+# Create a new directory structure
+sudo mkdir -p $BASE/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+sudo cp $REPO_BASE/packages/rpm/specs/$SPEC $BASE/SPECS/$SPEC
+
+# Copy over base
+sudo cp $REPO_BASE/$RELEASE $BASE/SOURCES
 
 # Use -bb option instead of -ba since we're not building from source and do not need a source rpm produced.
-rpmbuild -bb $BASE/SPECS/$SPEC
+sudo rpmbuild -bb $BASE/SPECS/$SPEC
+
+sudo sh -c "cp $BASE/RPMS/$ARCH/*.rpm $REPO_BASE"
+sudo chown $USER:$USER $REPO_BASE/*.rpm
+
+# Install it to make sure all works
+sudo rpm -Uvh $REPO_BASE/*.rpm
 
 # If you need it in another architecture instead of x86_64 then change the spec file and add BuildArch: <whatever here>
 # below Source0:
--- packages/rpm/specs/s3lsio.spec
+++ packages/rpm/specs/s3lsio.spec
@@ -1,13 +1,13 @@
 Name:    s3lsio
-Version: 0.1.7
-Release: 7%{?dist}
-Summary: AWS S3 and Ceph command-line utility
+Version: 0.1.9
+Release: 0%{?dist}
+Summary: AWS S3 and Ceph command-line utility and benchmarking.
 
 License: Apache2
 Source0: s3lsio
 
 %description
-S3lsio is a command-line utility to access AWS S3 and Ceph.
+S3lsio is a command-line utility to access AWS S3 and Ceph. Also includes benchmarking.
 
 %install
 install -p -m 0755 %{SOURCE0} %{_bindir}
